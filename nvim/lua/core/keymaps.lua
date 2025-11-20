4fb8bd02fe4ce98cd822a6762021f72560bb29a2
Brandon Castro <brandon.castro@nyu.edu>
2025-11-20 17:05:21 -0500

    Center single code window keybinding

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -14,3 +14,9 @@ vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set(
+	"n",
+	"<C-w>c",
+	":leftabove vertical new <bar> vertical resize 52<bar> set nornu<bar> wincmd l<CR>",
+	{ noremap = true, silent = true }
+)
